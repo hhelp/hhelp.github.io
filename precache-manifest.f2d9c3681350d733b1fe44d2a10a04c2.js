d08442982cfb51f334f95fdb62c89e52b29650e6
Ismayil-Mammadov <43794110+Ismayil-Mammadov@users.noreply.github.com>
2019-10-13 23:41:16 +0300

    Uploaded website

---
+++ precache-manifest.f2d9c3681350d733b1fe44d2a10a04c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e44699b5319947922d42949aa4b6a5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca9e74aab506386df14e",
+    "url": "/static/css/main.6790b903.chunk.css"
+  },
+  {
+    "revision": "0aa303d214c9c9b496b6",
+    "url": "/static/js/2.d3fe8a8c.chunk.js"
+  },
+  {
+    "revision": "ca9e74aab506386df14e",
+    "url": "/static/js/main.0d9e4420.chunk.js"
+  },
+  {
+    "revision": "b64a12af53b75d99ff8f",
+    "url": "/static/js/runtime-main.dd4f7b97.js"
+  },
+  {
+    "revision": "b23800837133ffd29e4a996f391a1b01",
+    "url": "/static/media/charity.b2380083.jpg"
+  }
+]);
