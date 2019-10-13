3efc6d62d2647b2800975b55ff6529e694047fee
Ismayil-Mammadov <43794110+Ismayil-Mammadov@users.noreply.github.com>
2019-10-13 23:05:22 +0300

    Add files via upload

---
+++ precache-manifest.a9f27ba2096d5dfb102d62abf778f81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cac044f8409f864e4232456948e6842",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f686ac9c70ef8131f6",
+    "url": "/static/css/main.57cb8b49.chunk.css"
+  },
+  {
+    "revision": "8d038c0f2456f42c68b8",
+    "url": "/static/js/2.4a4991ff.chunk.js"
+  },
+  {
+    "revision": "48f686ac9c70ef8131f6",
+    "url": "/static/js/main.9e05cd21.chunk.js"
+  },
+  {
+    "revision": "febe603a5a16f075e357",
+    "url": "/static/js/runtime-main.1c269599.js"
+  },
+  {
+    "revision": "b23800837133ffd29e4a996f391a1b01",
+    "url": "/static/media/charity.b2380083.jpg"
+  }
+]);
